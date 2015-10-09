5bc82542b5fab5e73c0e4cf5e5e328174260b577
Tsalsb <timothy.salsberry+git@gmail.com>
2015-10-08 21:10:00 -0700

    Update Folder-program.py

--- Folder-program.py
+++ Folder-program.py
@@ -13,7 +13,7 @@ def makefolders(root_dir, subfolders):
     concat_path = functools.partial(os.path.join, root_dir)
     map(os.makedirs, map(concat_path, subfolders))
 
-#Creation of folders according to the sylabus. 
+#Creation of folders according to the syllabus. 
 root_dir = myPath+"/ELECTR 155--ePortfolio/"# root folder 
 subfolders = ('Folder_1-Cover-Page', 'Folder_2-TOC', 'Folder_3-Student-Photo','Folder_4-Self-Introduction','Folder_5-Simple-Starter-Circuit','Folder_6-Circuit-40-2','Folder-7-Relay-Voltage-Comparator','Folder_8-ELENCO-PCB-Miniaturization','Folder_9-Reverse-Engineering-Project','Folder_10-Additional-Multisim','Folder_11-Summary-Page','Completed-Work')
 makefolders(root_dir, subfolders)
