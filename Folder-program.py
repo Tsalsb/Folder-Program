2b84e3060cebbae10d08d31699d6ed0c1d705735
Tsalsb <timothy.salsberry+git@gmail.com>
2015-10-08 20:17:57 -0700

    Update Folder-program.py

--- Folder-program.py
+++ Folder-program.py
@@ -1,2 +1,19 @@
-# Folder-Program
-A Script written in python to allow students to quickly Generate a folder with sub folders to the directory of the users choice 
+import os, sys
+import errno
+import functools
+import Tkinter, tkFileDialog
+root = Tkinter.Tk()
+#Starting path of the file explorer for Tkinter
+myPath = tkFileDialog.askdirectory(parent=root, initialdir="/",
+title='Please select where you would like the Directory created')
+
+
+#using functool we will create a working root dir and its subfolders
+def makefolders(root_dir, subfolders):
+    concat_path = functools.partial(os.path.join, root_dir)
+    map(os.makedirs, map(concat_path, subfolders))
+
+#Creation of folders according to the sylabus. 
+root_dir = myPath+"/ELECTR 155--ePortfolio/"# root folder 
+subfolders = ('Folder_1-Cover-Page', 'Folder_2-TOC', 'Folder_3-Student-Photo','Folder_4-Self-Introduction','Folder_5-Simple-Starter-Circuit','Folder_6-Circuit-40-2','Folder-7-Relay-Voltage-Comparator','Folder_8-ELENCO-PCB-Miniaturization','Folder_9-Reverse-Engineering-Project','Folder_10-Additional-Multisim','Folder_11-Summary-Page','Completed-Work')
+makefolders(root_dir, subfolders)
